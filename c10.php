82c8deb4435603957908b300f4b2dd883a3bdedc
Dane Powell <git@danepowell.com>
2014-10-14 15:51:37 -0500

    Finished challenge 10.

--- c10.php
+++ c10.php
@@ -2,9 +2,16 @@
 include 'crypt.php';
 
 $cipher_text = file_get_contents('10.txt');
-$key = 'YELLOW SUBMARINE';
 $cipher_text = base64_decode($cipher_text);
-$cipher_text = substr($cipher_text, 0, 16);
-$intermediate = ecb_decrypt($cipher_text, $key);
-echo $intermediate;
-//$plain_text = fxor($intermediate, $key);
+$key = 'YELLOW SUBMARINE';
+$block_size = strlen($key);
+$iv = str_pad("", $block_size, "\x00");
+
+$last_block_text = $iv;
+for ($i = 0; $i < strlen($cipher_text); $i = $i + $block_size) {
+  $block_text = substr($cipher_text, $i, $block_size);
+  $intermediate = ecb_decrypt($block_text, $key);
+  $plain_text = fxor($intermediate, $last_block_text);
+  $last_block_text = $block_text;
+  echo $plain_text;
+}
