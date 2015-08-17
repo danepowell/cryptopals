4ca94d3d131be5f5ce0ba7de49aeeb8b1a7572a4
Dane Powell <git@danepowell.com>
2015-08-17 09:58:15 -0500

    Added challenge 3.

--- python/crypt.py
+++ python/crypt.py
@@ -17,6 +17,22 @@ class CryptoTest(unittest.TestCase):
         out = fxor(bin_str1, bin_str2)
         self.assertEqual(out.encode('hex'), expected_hex_str)
 
+    def test_challenge_3(self):
+        input_hex_str = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+        bin_str = input_hex_str.decode('hex')
+        high_score = 0
+        score1 = 0
+        for i in range(128):
+            key = chr(i)
+            key_str = key * len(bin_str)
+            plaintext = fxor(bin_str, key_str)
+            score1 = score(plaintext)
+            print score1
+            print plaintext
+            if score1 > high_score:
+                print "candidate found: " + plaintext
+                high_score = score1
+
 def fxor(str1, str2):
     arr1 = array.array('B', str1)
     arr2 = array.array('B', str2)
@@ -24,6 +40,45 @@ def fxor(str1, str2):
     for i in range(len(arr1)):
         arr3.append(arr1[i] ^ arr2[i])
     return arr3.tostring()
+
+def score(str1):
+    str2 = str1.lower()
+    if (all(ord(c) < 128 for c in str1)):
+        return str2.count("e")
+    else:
+        return 0
+
+def letter_frequency():
+  return array(
+    'a' => 0.08167,
+    'b' => 0.01492,
+    'c' => 0.02782,
+    'd' => 0.04253,
+    'e' => 0.12702,
+    'f' => 0.02228,
+    'g' => 0.02015,
+    'h' => 0.06094,
+    'i' => 0.06966,
+    'j' => 0.00153,
+    'k' => 0.00772,
+    'l' => 0.04025,
+    'm' => 0.02406,
+    'n' => 0.06749,
+    'o' => 0.07507,
+    'p' => 0.01929,
+    'q' => 0.00095,
+    'r' => 0.05987,
+    's' => 0.06327,
+    't' => 0.09056,
+    'u' => 0.02758,
+    'v' => 0.00978,
+    'w' => 0.02360,
+    'x' => 0.00150,
+    'y' => 0.01974,
+    'z' => 0.00074,
+    ' ' => 0.21,
+  );
+}
 
 if __name__ == '__main__':
     unittest.main()
