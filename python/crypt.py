fe6c442e669aa552f527379d7106794c622744f1
Dane Powell <git@danepowell.com>
2017-05-06 19:33:27 -0700

    Added challenge 4.

--- python/crypt.py
+++ python/crypt.py
@@ -1,19 +1,47 @@
 import array
 
+# Fixed XOR: takes two equal-length binary strings and produces XOR combination.
 def fxor(str1, str2):
+    assert len(str1) == len(str2), "String lengths must be equal."
     arr1 = array.array('B', str1)
     arr2 = array.array('B', str2)
-    arr3 = array.array('B', '')
     for i in range(len(arr1)):
-        arr3.append(arr1[i] ^ arr2[i])
-    return arr3.tostring()
+        arr1[i] ^= arr2[i]
+    return arr1.tostring()
 
+# Guesses 1-byte XOR key for string.
+def guess_1byte_xor(str1):
+    str_len = len(str1)
+    scores = {}
+    texts = {}
+    for i in range(32, 126):
+        key = chr(i)
+        key_str = key * str_len
+        plaintext = fxor(str1, key_str)
+        texts[key] = plaintext
+        scores[key] = score(plaintext)
+    key = max(scores, key=scores.get)
+    high_score = scores[key]
+    text = texts[key]
+    # print 'key: ' + key
+    # print 'text: ' + text
+    return {'key': key, 'score': high_score, 'text': text}
+
+# Score a string based on enlish letter frequencies.
 def score(str1):
-    str2 = str1.lower()
-    if (all(ord(c) < 128 for c in str1)):
-        return str2.count("e")
-    else:
-        return 0
+    str1 = str1.lower()
+    frequencies = letter_frequencies()
+    ss = 0.0
+    for letter in frequencies.iterkeys():
+        expected_frequency = frequencies[letter]
+        actual_frequency = str1.count(letter)
+        ss += pow(actual_frequency - expected_frequency, 2)
+    return ss
 
+# Take hex-encoded string and convert to base64.
 def hex2b64(hex_str):
     return hex_str.decode('hex').encode('base64')
+
+# Return dict of english letter frequencies.
+def letter_frequencies():
+  return {'a':0.08167, 'b':0.01492, 'c':0.02782, 'd':0.04253, 'e':0.12702, 'f':0.02228, 'g':0.02015, 'h':0.06094, 'i':0.06966, 'j':0.00153, 'k':0.00772, 'l':0.04025, 'm':0.02406, 'n':0.06749, 'o':0.07507, 'p':0.01929, 'q':0.00095, 'r':0.05987, 's':0.06327, 't':0.09056, 'u':0.02758, 'v':0.00978, 'w':0.02360, 'x':0.00150, 'y':0.01974, 'z':0.00074, ' ':0.21}
--- python/test_crypt.py
+++ python/test_crypt.py
@@ -13,23 +13,24 @@ class CryptoTest(unittest.TestCase):
         input_hex_str1 = '1c0111001f010100061a024b53535009181c'
         input_hex_str2 = '686974207468652062756c6c277320657965'
         expected_hex_str = '746865206b696420646f6e277420706c6179'
-        bin_str1 = input_hex_str1.decode('hex')
-        bin_str2 = input_hex_str2.decode('hex')
-        out = crypt.fxor(bin_str1, bin_str2)
+        out = crypt.fxor(input_hex_str1.decode('hex'), input_hex_str2.decode('hex'))
         self.assertEqual(out.encode('hex'), expected_hex_str)
 
     def test_challenge_3(self):
         input_hex_str = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-        bin_str = input_hex_str.decode('hex')
-        high_score = 0
-        score1 = 0
-        for i in range(128):
-            key = chr(i)
-            key_str = key * len(bin_str)
-            plaintext = crypt.fxor(bin_str, key_str)
-            score1 = crypt.score(plaintext)
-            if score1 > high_score:
-                high_score = score1
+        input_bin_str = input_hex_str.decode('hex')
+        expected_key = 'X'
+        result = crypt.guess_1byte_xor(input_bin_str)
+        self.assertEqual(result['key'], expected_key)
+
+    def test_challenge_4(self):
+        cipher_lines = [line.rstrip('\n') for line in open('../resources/4.txt')]
+        plain_lines = {}
+        for cipher_line in cipher_lines:
+            plain_line = crypt.guess_1byte_xor(cipher_line.decode('hex'))
+            plain_lines[plain_line['score']] = {'key': plain_line['key'], 'text': plain_line['text']}
+        best_line = plain_lines[max(plain_lines)]
+        self.assertEqual(best_line['key'], '5')
 
 if __name__ == '__main__':
     unittest.main()
