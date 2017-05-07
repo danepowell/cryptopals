8181d54af7fbb154227d5a157a141351aa89f331
Dane Powell <git@danepowell.com>
2017-05-06 18:11:02 -0700

    More refactoring.

--- python/crypt.py
+++ python/crypt.py
@@ -1,36 +1,5 @@
-import unittest
 import array
 
-class CryptoTest(unittest.TestCase):
-
-    def test_challenge_1(self):
-        input_hex_str = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-        expected_b64_str = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t\n'
-        self.assertEqual(hex2b64(input_hex_str), expected_b64_str)
-
-    def test_challenge_2(self):
-        input_hex_str1 = '1c0111001f010100061a024b53535009181c'
-        input_hex_str2 = '686974207468652062756c6c277320657965'
-        expected_hex_str = '746865206b696420646f6e277420706c6179'
-        bin_str1 = input_hex_str1.decode('hex')
-        bin_str2 = input_hex_str2.decode('hex')
-        out = fxor(bin_str1, bin_str2)
-        self.assertEqual(out.encode('hex'), expected_hex_str)
-
-    def test_challenge_3(self):
-        input_hex_str = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-        bin_str = input_hex_str.decode('hex')
-        high_score = 0
-        score1 = 0
-        for i in range(128):
-            key = chr(i)
-            key_str = key * len(bin_str)
-            plaintext = fxor(bin_str, key_str)
-            score1 = score(plaintext)
-            if score1 > high_score:
-                high_score = score1
-
-# TODO: Move these into library.
 def fxor(str1, str2):
     arr1 = array.array('B', str1)
     arr2 = array.array('B', str2)
@@ -48,6 +17,3 @@ def score(str1):
 
 def hex2b64(hex_str):
     return hex_str.decode('hex').encode('base64')
-
-if __name__ == '__main__':
-    unittest.main()
